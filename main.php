83e7293b275e5c9e1b269b18d3b9addd87bd91a2
Aniket Sable <aniketsable9508@gmail.com>
2024-01-22 12:00:41 +0530

    second commit

--- main.php
+++ main.php
@@ -31,6 +31,20 @@
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
 </head>
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/65a67ba28d261e1b5f53ecb2/1hk955tel';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
 <body>
     <div class="container-xxl bg-white p-0">
